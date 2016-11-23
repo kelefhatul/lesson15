13d740f23111d1c22f179a736cb0de1b927c6431
kelefhatul <kostyantinromanov@gmail.com>
2016-11-23 15:09:16 +0200

    task05

--- big01/command/Command.java
+++ big01/command/Command.java
@@ -1,8 +1,5 @@
 package com.javarush.test.level31.lesson15.big01.command;
 
-/**
- * Created by root on 11/23/2016.
- */
 public interface Command {
     void execute() throws Exception;
 }
--- big01/command/ExitCommand.java
+++ big01/command/ExitCommand.java
@@ -1,11 +1,7 @@
 package com.javarush.test.level31.lesson15.big01.command;
 
-
 import com.javarush.test.level31.lesson15.big01.ConsoleHelper;
 
-/**
- * Created by root on 11/23/2016.
- */
 public class ExitCommand implements Command {
     @Override
     public void execute() throws Exception {
