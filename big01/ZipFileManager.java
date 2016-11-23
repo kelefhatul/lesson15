e9748afe42fe72308f0ed95055e0671dad6a5074
kelefhatul <kostyantinromanov@gmail.com>
2016-11-23 14:37:48 +0200

    task02

--- big01/ZipFileManager.java
+++ big01/ZipFileManager.java
@@ -1,6 +1,10 @@
 package com.javarush.test.level31.lesson15.big01;
 
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 /**
  * Created by root on 11/22/2016.
@@ -13,6 +17,25 @@ public class ZipFileManager {
     }
 
     public void createZip(Path source) throws Exception{
+        //1
+        try (ZipOutputStream zout = new ZipOutputStream(Files.newOutputStream(zipFile));) {
+            //2
+            ZipEntry zipEntry = new ZipEntry(source.getFileName().toString());
+            //3
+            zout.putNextEntry(zipEntry);
+            //4.1
+            try (InputStream fin = Files.newInputStream(source);) {
+                //4.2
+                int inData;
+                while ((inData = fin.read()) != -1) {
+                    zout.write(inData);
+                }
+            }
+            zout.closeEntry();
+        }
+
+
+
 
     }
 }
